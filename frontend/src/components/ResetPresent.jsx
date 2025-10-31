05ad25515b578930cf6bc11b576c5c5da605d856
SUJOY GARAI <sujoycode999@gmail.com>
2025-10-31 22:48:52 +0530

    update .env file ...

--- frontend/src/components/ResetPresent.jsx
+++ frontend/src/components/ResetPresent.jsx
@@ -12,7 +12,7 @@ export default function ResetPresent() {
 
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/reset_present", {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/reset_present`, {
         method: "POST",
       });
 
